c99b8a318817d3fe87610545e6d3f53d9a38bedf
CWestwood <westwoodcj@gmail.com>
2026-02-15 19:07:36 +0200

    Add multiplier for captain's score calculation

--- client/src/pages/TeamDetail.jsx
+++ client/src/pages/TeamDetail.jsx
@@ -282,7 +282,8 @@ export default function TeamDetail() {
   };
 
   const getPlayerDisplayScore = (player) => {
-
+    const multiplier = player.is_captain ? 2 : 1;
+    
     const baseTotal = (player.scores || []).reduce((sum, s) => sum + (Number(s.total_points) || 0), 0);
     const baseBatting = (player.scores || []).reduce((sum, s) => sum + (Number(s.batting_points) || 0), 0);
     const baseBowling = (player.scores || []).reduce((sum, s) => sum + (Number(s.bowling_points) || 0), 0);
