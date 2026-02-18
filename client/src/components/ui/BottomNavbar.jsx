d06e90473ba310c80df74e4be2c05662d6fa4a02
CWestwood <westwoodcj@gmail.com>
2026-02-18 21:21:01 +0200

    Update BottomNavbar.jsx

--- client/src/components/ui/BottomNavbar.jsx
+++ client/src/components/ui/BottomNavbar.jsx
@@ -62,9 +62,11 @@ const BottomNavbar = ({ onNavigate }) => {
   const sidebarOnlyItems = isRegistering
     ? [] // nothing extra during registration
     : [
+        { label: "Team Selection", route: "/team" },
         { label: "Tournament Rules", route: "/tournament-rules" },
         { label: "Substitution Log", route: "/substitutionlog" },
-        { label: "Tournament Tracker", route: "/tracker" }
+        { label: "Tournament Tracker", route: "/tracker" }, 
+        
 
       ];
 
