f57ab110ec2ec1f14afca9880783daf53a14a8b0
CWestwood <westwoodcj@gmail.com>
2026-02-19 20:42:48 +0200

    Fix substitution counts in TournamentRules table
    Corrected substitution counts for tournament placements.

--- client/src/pages/TournamentRules.jsx
+++ client/src/pages/TournamentRules.jsx
@@ -172,10 +172,10 @@ const TournamentRules = () => {
                       
              <table className="w-8/12 text-left text-sm bg-dark-600 p-4 rounded mx-auto mt-2 mb-4">
               <tbody className="divide-y divide-gray-700">
-                <tr><td className="p-3 text-sm text-center"> 1st - 5th </td><td className="p-3 text-center">1 sub</td></tr>
-                <tr><td className="p-3 text-sm text-center"> 6th - 10th</td><td className="p-3 text-center">2 subs</td></tr>
-                <tr><td className="p-3 text-sm text-center">11th - 15th</td><td className="p-3 text-center">3 subs</td></tr>
-                <tr><td className="p-3 text-sm text-center">&gt; 15th</td><td className="p-3 text-center">4 subs</td></tr>
+                <tr><td className="p-3 text-sm text-center"> 1st - 5th </td><td className="p-3 ttext-center"2 subs</td></tr>
+                <tr><td className="p-3 text-sm text-center"> 6th - 10th</td><td className="p-3 text-center">3 subs</td></tr>
+                <tr><td className="p-3 text-sm text-center">11th - 15th</td><td className="p-3 text-center">4 subs</td></tr>
+                <tr><td className="p-3 text-sm text-center">&gt; 15th</td><td className="p-3 text-center">5 subs</td></tr>
               </tbody>
              </table>
              <span className="text-sm text-red-600 block"> Unused Substitutions are not carried over from the Group Stage to the Super-8s </span> <br />
